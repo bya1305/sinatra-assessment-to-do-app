afa9f2e5a652c83fe4402e8bba365b559d77048e
Joshua Avina <s3nbonzakura78@gmail.com>
2016-07-04 18:33:42 +0000

    finished signup logic/added password_digest

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,55 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
-    erb :'user/signup'
+    if !logged_in?
+      erb :'user/signup'
+    else
+      redirect '/user_home'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect '/user_home'
+    end
+  end
+
+  get '/login' do
+
+  end
+
+  post '/login' do
+
+  end
+
+  get '/user_home' do
+    if logged_in?
+      erb :'user/user_home'
+    else
+      redirect '/'
+    end
+  end
+
+
+
+
+
+
+  helpers do
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
   end
 
 end
