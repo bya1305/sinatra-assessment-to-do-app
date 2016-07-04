e2afe4be685d1596387359ec3b684255fadde3c4
Joshua Avina <s3nbonzakura78@gmail.com>
2016-07-04 17:13:13 +0000

    fixed activerecord problem, finsihed home page & sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,19 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
 
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "Vu0=$LbdNf284&z3!/BwS7"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :'user/signup'
+  end
+
 end
